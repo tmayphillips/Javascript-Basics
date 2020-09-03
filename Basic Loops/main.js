78e9a2b74aabbbd2d52a5743a55de1e9ee779a3e
TIFFANY MAY-PHILLIPS <tiffany.phillips12@gmail.com>
2020-09-03 14:39:24 -0500

    first commit

---
+++ Basic Loops/main.js
@@ -0,0 +1,11 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy"
+];
+
+for (var i=0; i < todos.length; i++) {
+    console.log(todos[i]);
+}
